474579516730fb60010bfb6a68d1d81e1c47e4ec
Lucas Lima <lcs.freirelima@gmail.com>
2026-07-04 17:27:40 -0300

    Update mongodb to run in global mode and updates postgresql initialization script by removing db creation

--- databases/postgresql/01-users-dbs.sh
+++ databases/postgresql/01-users-dbs.sh
@@ -13,28 +13,4 @@ CREATE USER replicator WITH REPLICATION LOGIN PASSWORD '${REPLICATOR_PASSWORD}';
 CREATE USER pgpool WITH PASSWORD '${PGPOOL_PASSWORD}' LOGIN;
 GRANT pg_monitor TO pgpool;
 
--- Application users and databases
-CREATE USER npm WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE npm OWNER npm;
-
-CREATE USER outline WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE outline OWNER outline;
-
-CREATE USER vaultwarden WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE vaultwarden OWNER vaultwarden;
-
-CREATE USER seerr WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE seerr OWNER seerr;
-
-CREATE USER radarr WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE radarr OWNER radarr;
-CREATE DATABASE radarr_logs OWNER radarr;
-
-CREATE USER sonarr WITH PASSWORD 'change_me' LOGIN;
-CREATE DATABASE sonarr OWNER sonarr;
-CREATE DATABASE sonarr_logs OWNER sonarr;
-
-CREATE USER authelia WITH PASSWORD 'your_secure_password' LOGIN;
-CREATE DATABASE authelia OWNER authelia;
-
 EOSQL
